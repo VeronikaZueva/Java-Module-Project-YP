e80d7fe98bc7e676361d76f3c20d5b6ca95a834c
VeronikaZueva <zuevavero@yandex.ru>
2023-09-22 00:33:59 +0500

    Stage 1. Process of add count Person

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int countFriend = 0;
+
+        while (countFriend <= 1) {
+            System.out.println("На сколько человек разделить ваш счет?");
+            countFriend = scanner.nextInt();
+            if(countFriend == 1) {System.out.println("Нет смысла делить счет!");}
+            else {System.out.println("Введенное число некорректно.");}
+        }
+        System.out.println("Выполняем код дальше");
+
     }
 }
