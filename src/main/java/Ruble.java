9a96433b23988fc88e42266f83d4cc24f3919770
VeronikaZueva <zuevavero@yandex.ru>
2023-09-29 01:40:29 +0500

    Final Version

--- src/main/java/Ruble.java
+++ src/main/java/Ruble.java
@@ -2,25 +2,21 @@ public class Ruble {
 
     public String recieveRuble(double rublePrice) {
         int resultPrice = (int)Math.floor(rublePrice);
-        String ruble;
+
         if(resultPrice >= 11 && resultPrice <= 19) {
-            ruble = " рублей";
-            return ruble;
+            return " рублей";
         }
         else {
         int lastNumber = resultPrice % 10;
         switch(lastNumber) {
             case 1 :
-                ruble = " рубль";
-                return ruble;
+                return " рубль";
             case 2 :
             case 3 :
             case 4 :
-                ruble = " рубля";
-                return ruble;
+                return " рубля";
             default :
-                ruble = " рублей";
-                return ruble;
+                return " рублей";
 
         }
         }
